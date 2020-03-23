0d1043aa69643c9dd083ac8fd01bda28c050975f
David Olsen <davidols@buffalo.edu>
2020-03-23 09:55:16 -0400

    Restructuring Codebase and Adding Tests * added queries file and build folder constant * moved appleInfo constants to api directory * moved endpoint callbacks into separate file from server * ported node code to es6 * added jest and supertest to api directory

---
+++ api/constants.js
@@ -0,0 +1,57 @@
+export const appleInfo = [
+    {
+        id: 0,
+        name: "HoneyCrisp",
+        picture: "/apple-honeycrisp.png"
+    },
+    {
+        id: 1,
+        name: "Red Delicious",
+        picture: "/apple-red-delicious.png"
+    },
+    {
+        id: 2,
+        name: "Gala",
+        picture: "/apple-gala.png"
+    },
+    {
+        id: 3,
+        name: "Fuji",
+        picture: "/apple-fuji.png"
+    },
+    {
+        id: 4,
+        name: "Granny Smith",
+        picture: "/apple-grannysmith.png"
+    },
+    {
+        id: 5,
+        name: "Golden Delicious",
+        picture: "/apple-golden-delicious.png"
+    },
+    {
+        id: 6,
+        name: "Ambrosia",
+        picture: "/apple-ambrousia.png"
+    },
+    {
+        id: 7,
+        name: "McIntosh",
+        picture: "/apple-mcIntosh.png"
+    },
+    {
+        id: 8,
+        name: "Crispin",
+        picture: "/apple-crispin.png"
+    },
+    {
+        id: 9,
+        name: "JonaGold",
+        picture: "/apple-jonagold.png"
+    },
+    {
+        id: 10,
+        name: "Empire",
+        picture: "/apple-empire.png"
+    }
+]
